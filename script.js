2405d3ea7eadff40193af13f2139e94a9fd4d8d2
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-29 06:14:55 -0400

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// firstNumber operator secondNumber
+// i.e. 1 + 2
+let firstNumber;
+let secondNumber;
+let operator;
+
 function add(x, y) {
   return x + y;
 }
@@ -12,4 +18,11 @@ function multiply(x, y) {
 
 function divide(x, y) {
   return x / y;
+}
+
+function operate(x, y, op) {
+  if (op === "+") return add(x, y);
+  else if (op === "-") return subtract(x, y);
+  else if (op === "*") return multiply(x, y);
+  else if (op === "/") return divide(x, y);
 }
