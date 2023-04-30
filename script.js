4b254b6e27b70c133ad2a1cd9ceaacdf4e104a0e
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-30 00:53:04 -0400

    Add number display when pressed

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+const numberButtons = document.querySelectorAll(".num-key");
+const display = document.querySelector(".display");
+
+Array.from(numberButtons).forEach((numberButton) => {
+  numberButton.addEventListener("click", pressNumber);
+});
+
 function add(x, y) {
   return x + y;
 }
@@ -25,4 +32,13 @@ function operate(x, y, op) {
   else if (op === "-") return subtract(x, y);
   else if (op === "*") return multiply(x, y);
   else if (op === "/") return divide(x, y);
+}
+
+function pressNumber() {
+  firstNumber = `${firstNumber} + ${this.textContent}`;
+  updateDisplay(this.textContent);
+}
+
+function updateDisplay(newText) {
+  display.textContent = `${display.textContent}${newText}`;
 }
